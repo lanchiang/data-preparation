efc19cb994dcd4537cf086c1bbabeba2b6fd8204
John Koumarelas <John.Koumarel@gmail.com>
2018-01-31 13:33:52 +0100

    Initial commit of the new integrating project.

---
+++ src/main/scala/de/hpi/is/data_preparation/App.scala
@@ -0,0 +1,9 @@
+package de.hpi.is.data_preparation
+
+/**
+ * Hello world!
+ *
+ */
+object App extends Application {
+  println( "Hello World!" )
+}
---
+++ src/test/scala/de/hpi/is/data_preparation/AppTest.scala
@@ -0,0 +1,17 @@
+package de.hpi.is.data_preparation
+
+import org.junit._
+import Assert._
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
